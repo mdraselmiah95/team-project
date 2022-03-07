2db91a685fa3c703f4e98c21259958c038514cec
rasel <russelroy15@gmail.com>
2022-03-07 19:44:17 +0600

    new code added

--- src/components/sectionEight/SectionEight.js
+++ src/components/sectionEight/SectionEight.js
@@ -1,14 +1,14 @@
 const SectionEight = () => {
   return (
-    <div className="bg-cover bg-bg-eight mb-12">
-      <h1 className="text-4xl font-medium text-white  text-center pt-16">
+    <div className="bg-cover bg-bg-eight mb-12 lg:mx-28">
+      <h1 className="lg:text-4xl text-2xl font-medium text-white  text-center pt-16">
         I’m confused, I want a proper guideline
       </h1>
-      <div className="flex justify-center py-10">
-        <button className="bg-white hover:bg-gray-100 text-color-three font-bold py-2 px-4 rounded shadow mr-3">
+      <div className="flex justify-center pt-10 pb-14">
+        <button className="bg-white hover:bg-gray-100 text-color-three font-bold lg:py-4 lg:px-6 py-3 px-4 rounded shadow mr-3">
           Visit Our Support Center
         </button>
-        <button className="hover:bg-blue-400  hover:text-slate-600 text-white font-bold py-2 px-4 border border-white rounded shadow">
+        <button className="hover:bg-blue-400  hover:text-slate-600 text-white font-bold lg:py-4 lg:px-6 py-3 px-4  border border-white rounded shadow">
           All Courses
         </button>
       </div>

c73ea3c0ecd92017fd22940f375b7c4a8a7a4eed
mdraselmiah95 <russelroy15@gmail.com>
2022-04-06 05:15:24 +0600

    Course Details part two section complate

--- src/components/pages/courseDetails/courseInformation/CourseInformation.js
+++ src/components/pages/courseDetails/courseInformation/CourseInformation.js
@@ -1,16 +1,31 @@
 import React from "react";
+import { Outlet, Link, Routes, Route } from "react-router-dom";
+import CourseModule from "../courseModule/CourseModule";
+import CoursesOverview from "../coursesOverview/CoursesOverview";
 
 const CourseInformation = () => {
   return (
     <div>
-      <div className="flex">
-        <button className="px-6 py-2 mr-1 text-white rounded-t-md bg-color-five ">
+      <nav className="flex">
+        <Link
+          to="/courseDetails/body/information/courseOverview"
+          className="px-6 py-2 mr-1 text-white rounded-t-md bg-color-five"
+        >
           Courses Overview
-        </button>
-        <button className="px-6 py-2 mr-1 text-white rounded-t-md bg-color-five ">
+        </Link>
+
+        <Link
+          to="/courseDetails/body/information/courseModule"
+          className="px-6 py-2 mr-1 text-white rounded-t-md bg-color-five"
+        >
           Course Module
-        </button>
-      </div>
+        </Link>
+        <Outlet />
+      </nav>
+      <Routes>
+        <Route path="courseOverview" element={<CoursesOverview />} />
+        <Route path="courseModule" element={<CourseModule />} />
+      </Routes>
     </div>
   );
 };

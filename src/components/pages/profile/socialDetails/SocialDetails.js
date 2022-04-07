15bfc01d29da2dd018d88deabf0004aa9535b55e
mdraselmiah95 <russelroy15@gmail.com>
2022-04-07 19:29:53 +0600

    profile part complate

--- src/components/pages/profile/socialDetails/SocialDetails.js
+++ src/components/pages/profile/socialDetails/SocialDetails.js
@@ -10,7 +10,7 @@ const SocialDetails = () => {
   const gitHub =
     "https://res.cloudinary.com/mdraselmia/image/upload/v1649326201/social/Github_si5mw9.png";
   return (
-    <div className=" md:pb-32 md:px-36 px-6">
+    <div className=" md:pb-32 md:px-36 px-6 md:my-0 my-6">
       <h2 className="text-color-one font-medium text-2xl mb-6">Quiz Results</h2>
       <div className="flex">
         <a href="https://www.facebook.com/" className=" mr-5">

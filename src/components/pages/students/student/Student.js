e59f8992257e98358706d85053d6de9623bb4097
mdraselmiah95 <russelroy15@gmail.com>
2022-04-12 17:20:23 +0600

    new code added

--- src/components/pages/students/student/Student.js
+++ src/components/pages/students/student/Student.js
@@ -12,9 +12,26 @@ const Student = ({ data }) => {
     image,
   } = data;
   return (
-    <div>
+    <div className="flex">
       <img src={image} alt="student" />
-      <div></div>
+      <div className="ml-7">
+        <h2 className="mb-2 text-lg font-medium text-color-one">{name}</h2>
+        <h6 className="mb-5 text-color-two">{expertise}</h6>
+        <p className="mb-1 font-medium text-color-two">
+          Eligibility Test:
+          <span className="ml-1 font-bold text-color-eight">
+            {points} Points
+          </span>
+        </p>
+        <p className="mb-1 font-medium text-color-two">
+          Language:
+          <span className="ml-1 font-bold text-color-eight">{language}+</span>
+        </p>
+        <p className="font-medium text-color-two">
+          Completed Projects:
+          <span className="ml-1 font-bold text-color-eight">{projects}+</span>
+        </p>
+      </div>
     </div>
   );
 };

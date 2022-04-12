375b4d70b052e3a34c0544acd6b02f7681a861c0
mdraselmiah95 <russelroy15@gmail.com>
2022-04-12 17:06:46 +0600

    new code added

---
+++ src/components/pages/students/student/Student.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Student = () => {
+  return (
+    <div>
+      <h2>ee</h2>
+    </div>
+  );
+};
+
+export default Student;
--- src/components/pages/students/studentsBody/StudentsBody.js
+++ src/components/pages/students/studentsBody/StudentsBody.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Student from "../student/Student";
 
 const StudentsBody = () => {
+  const [students, setStudents] = useState([]);
+
+  useEffect(() => {
+    fetch("./coursesDetails.json")
+      .then((res) => res.json())
+      .then((data) => setStudents(data));
+  }, []);
   return (
-    <div>
-      <h1>g</h1>
+    <div className="px-6 py-8 lg:py-28 lg:px-36 ">
+      <div className="items-center justify-between flex-none md:flex">
+        <div className="mb-8 text-center md:mb-0 md:text-left coursesTitle">
+          <h1 className="text-xl font-medium md:text-4xl text-color-one">
+            Eligible Students for Hire
+          </h1>
+          <p className="mt-5 text-color-two">
+            We believe in technology and our team to take care of your career
+            journey. We guarantee you will get the best service that you have
+            never experienced before
+          </p>
+        </div>
+
+        <div className="flex">
+          <input
+            type="text"
+            placeholder="Search"
+            className="px-4 py-2 mr-1 border-2 rounded-md cursor-pointer md:px-6 md:py-3 border-color-two placeholder:text-color-two"
+          />
+          <button className="px-4 py-2 font-bold text-white border rounded-md shadow md:px-6 md:py-3 bg-color-one hover:bg-white hover:border-color-three hover:border hover:text-color-three ">
+            Search Now
+          </button>
+        </div>
+      </div>
+      <div className="mt-12">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+          {students.map((data) => (
+            <Student key={data.id} data={data} />
+          ))}
+        </div>
+      </div>
     </div>
   );
 };

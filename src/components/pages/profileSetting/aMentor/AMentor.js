e8406a72b42a95a1c9665e4b386bec0d82d20420
Shakiba448 <shakiba448@gmail.com>
2022-04-15 01:12:55 +0600

    WIP done mentor

--- src/components/pages/profileSetting/aMentor/AMentor.js
+++ src/components/pages/profileSetting/aMentor/AMentor.js
@@ -1,37 +1,44 @@
 import React from "react";
 import "./AMentor.css";
-const AMentor = () => {
+
+const AMentor = ({ mentor, setMentor }) => {
   return (
-    <div className="px-4 md:px-36">
+    <div className="px-4 md:px-36 mb-8">
       <div className="flex items-center justify-between px-6 py-12 md:py-20 md:px-14 bg-color-four rounded-2xl">
         <h1 className="text-4xl font-medium text-color-one">
           Are your a mentor?
         </h1>
         <div className="flex items-center">
-          <label
-            for="toggle"
-            className="text-2xl font-medium cursor-pointer text-color-one"
+          <div
+            htmlFor="toggle"
+            className={`text-2xl font-medium cursor-pointer text-color-one p-2 ${
+              !!mentor ? "bg-white " : "bg-red-300"
+            }`}
+            onClick={() => setMentor(false)}
           >
             No
-          </label>
-          <div className="relative inline-block w-16 mx-3 align-middle transition ease-in select-none duration-300">
+          </div>
+          {/* <div className="relative  inline-block w-16 mx-3 align-middle transition ease-in select-none duration-300">
             <input
               type="checkbox"
               name="toggle"
               id="toggle"
-              className="absolute block w-6 h-6 bg-white rounded-full appearance-none cursor-pointer toggle-checkbox"
+              className="absolute  block w-6 h-6 bg-white rounded-full appearance-none cursor-pointer toggle-checkbox"
             />
             <label
-              for="toggle"
+              htmlFor="toggle"
               className="block h-6 overflow-hidden bg-gray-300 rounded-full cursor-pointer toggle-label"
-            ></label>
-          </div>
-          <label
-            for="toggle"
-            className="text-2xl font-medium cursor-pointer text-color-one"
+            />
+          </div> */}
+          <div
+            htmlFor="toggle"
+            className={`text-2xl font-medium cursor-pointer text-color-one p-2 ${
+              mentor ? "bg-green-300" : "bg-white"
+            }`}
+            onClick={() => setMentor(true)}
           >
             Yes
-          </label>
+          </div>
         </div>
       </div>
     </div>

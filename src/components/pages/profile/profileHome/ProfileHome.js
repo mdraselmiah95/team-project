451348f4ce7ef5546c447736c8eae481ac3de585
Shakiba448 <shakiba448@gmail.com>
2022-04-30 21:26:49 +0600

    WIP done profile fetch

--- src/components/pages/profile/profileHome/ProfileHome.js
+++ src/components/pages/profile/profileHome/ProfileHome.js
@@ -12,7 +12,7 @@ const ProfileHome = () => {
   const url = "https://lxnpjwwijxqnrluhcfsr.nhost.run/v1/graphql";
   const user = authStore((state) => state.user);
   const dispatch = authStore((state) => state.dispatch);
-  useQuery("userProfile", async () => {
+  useQuery(["userDetails", user.userInfo?.id], async () => {
     const { data } = await axios({
       url: url,
       headers: {
--- src/components/pages/profile/socialDetails/SocialDetails.js
+++ src/components/pages/profile/socialDetails/SocialDetails.js
@@ -1,6 +1,9 @@
 import React from "react";
+import authStore from "../../../../utils/Store";
 
 const SocialDetails = () => {
+  const userDetails = authStore((state) => state.userDetails);
+
   const facebook =
     "https://res.cloudinary.com/mdraselmia/image/upload/v1649326201/social/Facebook_xpxn6k.png";
   const linkedin =
@@ -11,20 +14,51 @@ const SocialDetails = () => {
     "https://res.cloudinary.com/mdraselmia/image/upload/v1649326201/social/Github_si5mw9.png";
   return (
     <div className=" md:pb-32 md:px-36 px-6 md:my-0 my-6">
-      <h2 className="text-color-one font-medium text-2xl mb-6">Quiz Results</h2>
+      <h2 className="text-color-one font-medium text-2xl mb-6">
+        Social Connection
+      </h2>
       <div className="flex">
-        <a href="https://www.facebook.com/" className=" mr-5">
-          <img src={facebook} alt="facebook" />
-        </a>
-        <a href="https://www.linkedin.com/feed/" className=" mr-5">
-          <img src={linkedin} alt="linkedin" />
-        </a>
-        <a href="https://www.behance.net/" className=" mr-5">
-          <img src={behance} alt="behance" />
-        </a>
-        <a href="https://github.com/" className=" mr-5">
-          <img src={gitHub} alt="gitHub" />
-        </a>
+        {userDetails?.facebook && (
+          <a
+            href={userDetails?.facebook}
+            className=" mr-5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={facebook} alt="facebook" />
+          </a>
+        )}
+        {userDetails?.linkedin && (
+          <a
+            href={userDetails?.linkedin}
+            className=" mr-5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={linkedin} alt="linkedin" />
+          </a>
+        )}
+        {userDetails?.behance && (
+          <a
+            href={userDetails?.behance}
+            className=" mr-5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={behance} alt="linkedin" />
+          </a>
+        )}
+
+        {userDetails?.github && (
+          <a
+            href={userDetails?.github}
+            className=" mr-5"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img src={gitHub} alt="gitHub" />
+          </a>
+        )}
       </div>
     </div>
   );

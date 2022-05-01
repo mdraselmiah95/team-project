cd436936d89eb31637f39f265fa0f0ac0f46ae00
mdraselmiah95 <russelroy15@gmail.com>
2022-05-01 23:57:09 +0600

    new design added

--- src/components/pages/profile/sectionTwo/SectionTwo.js
+++ src/components/pages/profile/sectionTwo/SectionTwo.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SectionTwo.css";
+import ReactPlayer from "react-player/youtube";
 import authStore from "../../../../utils/Store";
 
 const SectionTwo = () => {
+  const [video, setVideoBtn] = useState(false);
   const user = authStore((state) => state.user);
   const userDetails = authStore((state) => state.userDetails);
   const img = "https://i.ibb.co/tw0jffZ/Group.png";
   const imgTwo = "https://i.ibb.co/Qvv15bs/play.png";
+  const url = "https://www.youtube.com/watch?v=qz0aGYrrlhU&t=11s";
   return (
     <div className=" grid md:grid-cols-2 items-center justify-center px-5 py-7 md:px-36">
       <div className="md:mr-14 md:mt-28">
@@ -16,19 +19,30 @@ const SectionTwo = () => {
         <p className="text-color-two">{userDetails?.description}</p>
       </div>
       <div className="relative bg-black rounded-xl md:-mt-96 md:ml-28">
-        <img
-          src={img}
-          alt="videoImage"
-          className="object-fill opacity-50 cursor-pointer"
-        />
-        <button className="btn">
-          <img
-            src={imgTwo}
-            alt="play icon"
-            style={{ height: "75px" }}
-            // onClick={() => setShowModal(false)}
+        {!video ? (
+          <>
+            <img
+              src={img}
+              alt="icon"
+              className="object-fill w-full h-full opacity-50 cursor-pointer "
+              type="button"
+            />
+
+            <button className="btn" onClick={() => setVideoBtn(true)}>
+              <img src={imgTwo} alt="play icon" style={{ height: "75px" }} />
+            </button>
+          </>
+        ) : (
+          <ReactPlayer
+            url={url}
+            onError={(err) => console.log(err)}
+            onEnded={() => setVideoBtn(false)}
+            controls
+            playing
+            width="422px"
+            height="422px"
           />
-        </button>
+        )}
       </div>
     </div>
   );

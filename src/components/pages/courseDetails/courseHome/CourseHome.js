354a4dcceaea4eb08de8431a21ff907721f1a270
Shakiba448 <shakiba448@gmail.com>
2022-05-08 12:27:04 +0600

    WIP on details

--- src/components/pages/courseDetails/courseHome/CourseHome.js
+++ src/components/pages/courseDetails/courseHome/CourseHome.js
@@ -4,26 +4,28 @@ import CourseBody from "../courseBody/CourseBody";
 import "./CourseHome.css";
 import { useParams } from "react-router-dom";
 import { fakeData } from "../../../../data/fakeData";
-import { useQuery } from "react-query";
+import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 
 const CourseHome = () => {
   const { id } = useParams();
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
   // const { data } = queryClient.getQueryData("products");
 
   const url = "https://lxnpjwwijxqnrluhcfsr.nhost.run/v1/graphql";
 
-  const { data, isLoading } = useQuery(["products", id], async () => {
-    const { data } = await axios({
-      url: url,
-      headers: {
-        "Content-Type": "application/json",
-        "x-hasura-admin-secret": "3a590f26c50099fdc779b212c090c1bf",
-      },
-      method: "POST",
-      data: {
-        query: `
+  const { data, isLoading } = useQuery(
+    ["products", id],
+    async () => {
+      const { data } = await axios({
+        url: url,
+        headers: {
+          "Content-Type": "application/json",
+          "x-hasura-admin-secret": "3a590f26c50099fdc779b212c090c1bf",
+        },
+        method: "POST",
+        data: {
+          query: `
         {
           products {
             base64
@@ -45,19 +47,20 @@ const CourseHome = () => {
             wholePrice
           }
         }`,
-      },
-    });
-    return data;
-  });
+        },
+      });
+      return data;
+    },
+    {
+      initialData: () => queryClient.getQueryData("products"),
+    }
+  );
   const newAddedProducts = fakeData.concat(
     data?.data?.products.map((item) => item)
   );
-  // const newAddedProducts = fakeData.concat(data?.products.map((item) => item));
   const findProduct = isLoading
     ? fakeData?.find((item) => item.id === id)
     : newAddedProducts?.find((item) => item.id === id);
-
-  // const findProduct = newAddedProducts?.find((item) => item.id === id);
 
   return (
     <div className="responsive">
